26ac596c35f2d096eb0960256a75e537eae47692
Alex George <107772948+algx23@users.noreply.github.com>
2025-12-19 18:00:09 +0000

    Fix: implement calculate_mean_and_standard_deviation
    New function calculate_mean_and_standard_deviation replaces
    calculate_mean_standard_dev to properly calculate the mean, and standard
    deviation of the dataset to be used with Pytorch Normalize function
    
    Changes:
    - Utils.py
      - calculate_mean_and_standard_deviation implemented to calculate the mean
        per batch, and the standard deviation using the expression:
        sqrt(E[X^2] - E[X]^2) (wikipedia)
      - torch.mean applied straight onto the batch "training_features" which
        calculates the mean using all pixel values in the image tensor anyway
        which can speed up execution
    
    - main.py
      - change batch size from 24 to 25 to get even batch sizes -> makes
        calculating the batch means easier since all batches have the same
        weight because they have the same number of images

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from chexpert_dataset import ChexpertDataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import Resize # to resize all images
-from utils import calculate_mean_standard_dev
+from utils import calculate_mean_and_standard_deviation
 
 resize_transform = Resize((224, 224)) # some images are different sizes so resize them all to the same size
 dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=resize_transform)
@@ -16,8 +16,9 @@ image, labels = dataset[0]
 #print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
 #print(f"labels: {labels.shape} || {labels}\n") # checking the labels exist properly for the image
 
-dataset_loader = DataLoader(dataset, batch_size=24, shuffle=True)
-calculate_mean_standard_dev(dataset_loader)
+dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
+mean, standard_deviation = calculate_mean_and_standard_deviation(dataset_loader)
+print(f"mean = {mean}, standard deviation = {standard_deviation}")
 
 
 # checking the data is loaded - from PyTorch DataLoader Documentation
--- src/utils.py
+++ src/utils.py
@@ -1,38 +1,45 @@
 from torch import tensor, float32
-def calculate_mean_standard_dev(dataloader):
-    num_batches = len(dataloader)
-    sum_of_pixels = 0
-    print(f"num batches: {num_batches}")
+from math import sqrt
+import torch
 
 
-    # go through every batch in the dataloader 
-    for batch_num, (train_features, train_labels) in enumerate(dataloader):
+def calculate_mean_and_standard_deviation(dataloader):
+    """
+    get the mean pixel value, and standard deviation for the entire dataset
 
-        batch_size = len(train_features)
-        # store a sum of the mean for the batch
-        batch_mean_sum = 0
-
-        # testing with the first batch only for now
-        if batch_num != 0:
-            break
-        print(f"batch number: {batch_num}")
-        print(f"train features size: {train_features.size()}") # [batch_size, num channels, H, W]
+    References:
+    - https://www.youtube.com/watch?time_continue=359&v=y6IEcEBRZks&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fsca_esv%3Ddb2aaf917c165eff%26udm%3D7%26q%3Dwork%2Bout%2Bstandard%2Bdeviation%2Bpytorch%26sa%3DX%26ved%3D2ahUKEwja0LajisqRAxW&source_ve_path=MzY4NDIsMjM4NTE
+    - https://en.wikipedia.org/wiki/Standard_deviation to understand more where the formula used comes from
 
 
-        # for each image tensor in the batch - each index is a new image
-        # loop through, get the image, and calculate the mean of its pixel values
-        # and add it to the sum for the batch, and divide by batch size to get mean
-        for i in range(batch_size):
-            print(f"train features: {train_features[i].size()}") # a 224 x 224 grayscale img
-            print(f"image number: {i + 1}") # index starts at 0
-            image = train_features[i].squeeze()
-            print(f"IMAGE TENSOR: {image.size()}")
-            image = image.to(dtype=float32)
-            image_mean = image.mean()
-            print(f"image mean = {image_mean}")
-            batch_mean_sum += image_mean
+    Arguments: dataloader: the dataloader which contains the dataset
+
+    Returns: (mean: float, standard_deviation: float)
+    """
+    sum_pixel_vals = 0
+    sum_pixel_vals_squared = 0
+    batches_amount = len(dataloader)
 
 
-        print(f"sum of means for the first batch = {batch_mean_sum}")
-        batch_mean = batch_mean_sum / batch_size
-        print(f"the mean for all images in the first batch is {batch_mean}")
+    # As per wikipedia X is some variable - for me it is the pixel value
+    # for each batch of the dataset, calculate the mean pixel value for each batch
+    # and divide by the number of batches to get the overall mean for the dataset E[X]
+    # as per wikipedia
+    # for standard deviation:
+    # square each pixel value in the batch, and find the mean for this squared batch
+    # E[X^2] as per wikipedia
+    # find the variance -> E[X^2] - E[X]^2 as per wikipedia
+    # this is the variance -> square root to get the standard deviation
+    for train_features, train_labels in dataloader:
+        # print(train_features.size()) # a batch of 25, 224x224 images [batch size, channels (1 because grayscale), height, width
+        train_features = train_features.to(dtype=float32)
+        sum_pixel_vals += torch.mean(train_features) # calculate the mean for every image in the batch - uses every value in the batch
+        sum_pixel_vals_squared += torch.mean(train_features ** 2)
+
+    mean = sum_pixel_vals / batches_amount # E[X] on wikipedia
+    variance = (sum_pixel_vals_squared / batches_amount - mean ** 2) # E[X^2] on wikipedia
+    standard_deviation = sqrt(variance)
+
+    #print(mean, standard_deviation)
+
+    return (mean, standard_deviation)
