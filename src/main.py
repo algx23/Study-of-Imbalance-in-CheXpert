eb48c24803bf2ab0b979f472de05974f4ce451a3
Alex George <107772948+algx23@users.noreply.github.com>
2026-01-01 16:40:30 +0000

    Split subset into training and validation set
    Subset.csv now further split into training and validation sets, with a 90/10 train/validation split

--- src/main.py
+++ src/main.py
@@ -3,11 +3,30 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose,Normalize, Resize # to resize all images
 from utils import calculate_mean_and_standard_deviation
 
+<<<<<<< Updated upstream
 resize_transform = Resize((224, 224)) # some images are different sizes so resize them all to the same size
 dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=resize_transform)
 
 
 print(f"dataset size: {dataset.__len__()}")
+=======
+from model import BaselineModel 
+from torch.optim import Adam
+from torch.nn import BCEWithLogitsLoss
+from constants import NUM_EPOCHS
+
+import matplotlib.pyplot as plt
+from utils import plot_training_loss
+
+def prepare_data():
+
+    resize_transform = Resize((224, 224)) # some images are different sizes so resize them all to the same size
+    train_dataset = ChexpertDataset("train.csv", "D:/dataset fyp/", transform=resize_transform)
+    validation_dataset = ChexpertDataset("validation.csv", "D:/dataset fyp/", transform=resize_transform)
+
+
+    print(f"dataset size: {train_dataset.__len__()}")
+>>>>>>> Stashed changes
 
 # testing with just 1 image for now to test logic
 #image, labels = dataset[0]
@@ -16,6 +35,7 @@ print(f"dataset size: {dataset.__len__()}")
 #print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
 #print(f"labels: {labels.shape} || {labels}\n") # checking the labels exist properly for the image
 
+<<<<<<< Updated upstream
 dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
 
 
@@ -26,6 +46,16 @@ mean = 0.5062767267227173
 standard_deviation = 0.28674584034677253
 
 
+=======
+    dataset_loader = DataLoader(train_dataset, batch_size=25, shuffle=True)
+
+
+
+    ####### TEMP: Temporarily hard code mean and std during testing
+    mean, standard_deviation = calculate_mean_and_standard_deviation(dataset_loader)
+    #mean = 0.5062767267227173
+    #standard_deviation = 0.28674584034677253
+>>>>>>> Stashed changes
 
 print(f"mean = {mean}, standard deviation = {standard_deviation}")
 
@@ -34,6 +64,7 @@ transforms = Compose([
     Normalize(mean=mean, std=standard_deviation)
 ])
 
+<<<<<<< Updated upstream
 after_normalization_dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=transforms)
 after_normalization_loader = DataLoader(after_normalization_dataset, batch_size=25, shuffle=True) 
 
@@ -41,6 +72,16 @@ after_normalization_loader = DataLoader(after_normalization_dataset, batch_size=
 image, labels = after_normalization_dataset[0]
 print(f"label type: {type(labels)}")
 print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
+=======
+    after_normalization_train_dataset = ChexpertDataset("train.csv", "D:/dataset fyp/", transform=transforms)
+    after_normalization_validation_dataset = ChexpertDataset("validation.csv", "D:/dataset fyp/", transform=transforms)
+    after_normalization_loader = DataLoader(after_normalization_train_dataset, batch_size=25, shuffle=True) 
+
+    # checking the after normalization dataset
+    image, labels = after_normalization_train_dataset[0]
+    print(f"label type: {type(labels)}")
+    print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
+>>>>>>> Stashed changes
 
 # check the range of values after normalizing
 print(f"max pixel value: {image.max()}, min pixel value: {image.min()}")
