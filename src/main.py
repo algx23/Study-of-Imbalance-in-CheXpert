c4354031ae506679a49280576361d1413cd704a6
Alex George <107772948+algx23@users.noreply.github.com>
2026-01-01 17:46:59 +0000

    Fix merge conflict issues from commit commit 637bcf01b75a64fbfdb51e68194c38ff9da02e69

--- src/main.py
+++ src/main.py
@@ -1,30 +1,10 @@
 import torch
-
 from chexpert_dataset import ChexpertDataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose,Normalize, Resize # to resize all images
 from utils import calculate_mean_and_standard_deviation
 
-from model import BaselineModel 
-from torch.optim import Adam
-from torch.nn import BCEWithLogitsLoss
-from constants import NUM_EPOCHS
-
-import matplotlib.pyplot as plt
-from utils import plot_training_loss
-
-def prepare_data():
-
-    resize_transform = Resize((224, 224)) # some images are different sizes so resize them all to the same size
-    dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=resize_transform)
-<<<<<<< Updated upstream
-resize_transform = Resize((224, 224)) # some images are different sizes so resize them all to the same size
-dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=resize_transform)
-
-
-print(f"dataset size: {dataset.__len__()}")
-=======
-from model import BaselineModel 
+from model import BaselineModel
 from torch.optim import Adam
 from torch.nn import BCEWithLogitsLoss
 from constants import NUM_EPOCHS
@@ -40,7 +20,6 @@ def prepare_data():
 
 
     print(f"dataset size: {train_dataset.__len__()}")
->>>>>>> Stashed changes
 
     # testing with just 1 image for now to test logic
     #image, labels = dataset[0]
@@ -49,19 +28,6 @@ def prepare_data():
     #print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
     #print(f"labels: {labels.shape} || {labels}\n") # checking the labels exist properly for the image
 
-<<<<<<< Updated upstream
-dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
-    dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
-
-
-
-    ####### TEMP: Temporarily hard code mean and std during testing
-    #mean, standard_deviation = calculate_mean_and_standard_deviation(dataset_loader)
-    mean = 0.5062767267227173
-    standard_deviation = 0.28674584034677253
-
-
-=======
     dataset_loader = DataLoader(train_dataset, batch_size=25, shuffle=True)
 
 
@@ -70,7 +36,6 @@ dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
     mean, standard_deviation = calculate_mean_and_standard_deviation(dataset_loader)
     #mean = 0.5062767267227173
     #standard_deviation = 0.28674584034677253
->>>>>>> Stashed changes
 
     print(f"mean = {mean}, standard deviation = {standard_deviation}")
 
@@ -79,24 +44,14 @@ dataset_loader = DataLoader(dataset, batch_size=25, shuffle=True)
         Normalize(mean=mean, std=standard_deviation)
     ])
 
-<<<<<<< Updated upstream
-after_normalization_dataset = ChexpertDataset("subset.csv", "D:/dataset fyp/", transform=transforms)
-after_normalization_loader = DataLoader(after_normalization_dataset, batch_size=25, shuffle=True) 
-
-    # checking the after normalization dataset
-    image, labels = after_normalization_dataset[0]
-    print(f"label type: {type(labels)}")
-    print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
-=======
     after_normalization_train_dataset = ChexpertDataset("train.csv", "D:/dataset fyp/", transform=transforms)
     after_normalization_validation_dataset = ChexpertDataset("validation.csv", "D:/dataset fyp/", transform=transforms)
-    after_normalization_loader = DataLoader(after_normalization_train_dataset, batch_size=25, shuffle=True) 
+    after_normalization_loader = DataLoader(after_normalization_train_dataset, batch_size=25, shuffle=True)
 
     # checking the after normalization dataset
     image, labels = after_normalization_train_dataset[0]
     print(f"label type: {type(labels)}")
     print(f"image shape: {image.shape}\n image : {image}\n") # grayscale(1 channel) 224x224 image
->>>>>>> Stashed changes
 
     # check the range of values after normalizing
     print(f"max pixel value: {image.max()}, min pixel value: {image.min()}")
@@ -142,23 +97,23 @@ def train_model(model, dataloader):
             total_loss_for_epoch += loss.item()
             current_batch_num += 1
 
-            if i % 50  == 49: # print the loss every 10 batches
+            if i % 50 == 49: # print the loss every 10 batches
                 loss_so_far = total_loss_for_epoch / current_batch_num # avg loss up to current batch number
                 print(f"loss so far at batch {current_batch_num}: {loss_so_far}")
 
         # after each epoch during training, add the epoch number and loss value
         # to the list to be visualized
 
-        epoch_average_loss =  total_loss_for_epoch/(current_batch_num) # average loss total so far up to the current epoch
+        epoch_average_loss = total_loss_for_epoch/(current_batch_num) # average loss total so far up to the current epoch
         epoch_list.append(epoch+1)
         loss_to_plot.append(epoch_average_loss)
 
 
         print(f"Epoch {epoch+1} complete. Final Avg Loss for this epoch: {epoch_average_loss}") # average loss across all batches
-
+    
     print(f"training completed")
     return (model, loss_to_plot, epoch_list)
-       
+
 
 if __name__ == "__main__":
     dataloader_for_training = prepare_data()
@@ -168,6 +123,3 @@ if __name__ == "__main__":
     post_train_model, losses, epochs = train_model(model, dataloader_for_training)
     torch.save(post_train_model.state_dict(), "baseline_test.pt")
     plot_training_loss(losses, epochs)
-
-    
-
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,8 @@
 import torch
 import pandas as pd
 
+# print("hello I'm testing pytorch and Pandas!")
+# print(f"PyTorch version: {torch.__version__}")
+# print(f"Pandas version:  {pd.__version__}")
 
-print("hello I'm testing pytorch and Pandas!")
-print(f"PyTorch version: {torch.__version__}")
-print(f"Pandas version:  {pd.__version__}")
+#TODO: load dataset
