f153b2ebe884318565f6379acc90eee04af24d0c
Alex George <107772948+algx23@users.noreply.github.com>
2026-01-18 19:31:12 +0000

    fix: validation dataset transforms now different to train set
    Prior to this fix, the transforms applied to the validation set were the
    same as those applied to the training set.
    
    Now, validation transforms do not include the augmentations, as the
    validation set represents some unseen, real world data from which i can
    see an indication how well my model will generalize to the final test
    set.
    
    Other small changes:
    - losses_to_plot renamed to train_losses for increased readability and
      clarity
    - Learning rate of Adam Optimizer lowered to 1e-4 to help the model
      improve more - previously, validation loss was increasing after only 3
      or so epochs, meaning the baseline might be less informative

--- src/main.py
+++ src/main.py
@@ -26,11 +26,15 @@ from utils import plot_loss
 
 from pathlib import Path
 
-from sklearn.metrics import multilabel_confusion_matrix, classification_report, ConfusionMatrixDisplay
+from sklearn.metrics import (multilabel_confusion_matrix,
+                             classification_report,
+                             roc_auc_score,
+                             ConfusionMatrixDisplay)
 import numpy as np
 
 # augmentation imports
-from torchvision.transforms import RandomRotation, RandomHorizontalFlip
+from torchvision.transforms import (RandomRotation,
+                                    RandomHorizontalFlip)
 
 
 def prepare_data():
@@ -65,9 +69,14 @@ def prepare_data():
         Normalize(mean=mean, std=standard_deviation)
     ])
 
+    validation_transforms = Compose([
+        resize_transform,
+        Normalize(mean=mean, std=standard_deviation)
+    ])
+
     after_normalization_train_dataset = ChexpertDataset("train.csv", "D:/dataset fyp/", transform=transforms)
-    after_normalization_validation_dataset = ChexpertDataset("validation.csv", "D:/dataset fyp/", transform=transforms)
     after_normalization_train_loader = DataLoader(after_normalization_train_dataset, batch_size=25, shuffle=True)
+    after_normalization_validation_dataset = ChexpertDataset("validation.csv", "D:/dataset fyp/", transform=validation_transforms)
     after_normalization_validation_loader = DataLoader(after_normalization_validation_dataset, batch_size=25, shuffle=True)
 
     # checking the after normalization dataset
@@ -118,9 +127,9 @@ def train_model(model, train_dataloader, validation_dataloader, class_weights=No
         validation_loader = validation_dataloader
     )
     loss_function = BCEWithLogitsLoss(pos_weight=class_weights)
-    optimizer = Adam(model.parameters())
+    optimizer = Adam(model.parameters(), lr=0.0001)
 
-    loss_to_plot = [] # list of loss values to plot
+    train_losses = [] # list of loss values to plot
     epoch_list = [] # corresponding epoch of each loss value during training
 
     loss_so_far = 0
@@ -152,24 +161,28 @@ def train_model(model, train_dataloader, validation_dataloader, class_weights=No
         # to the list to be visualized
         epoch_average_loss = total_loss_for_epoch/(current_batch_num) # average loss total so far up to the current epoch
         epoch_list.append(epoch+1)
-        loss_to_plot.append(epoch_average_loss)
+        train_losses.append(epoch_average_loss)
 
         if validation_loss_checker.training_should_stop(model):
             print(f"NOT ENOUGH IMPROVEMENT FOUND, STOPPING TRAINING")
-            write_train_loss_to_file(epoch_list, loss_to_plot)
+            write_train_loss_to_file(epoch_list, train_losses)
             validation_losses = validation_loss_checker.current_losses
             print(f"MODEL TO BE USED FROM EPOCH: {validation_loss_checker.epoch_of_saved_model}")
-            return(loss_to_plot, validation_losses, epoch_list)
+
+            model.load_state_dict(torch.load(f"{MODEL_NAME}/{MODEL_NAME}.pt"))
+            validation_loss_checker.plot_pr_curve(model)
+
+            return(train_losses, validation_losses, epoch_list)
 
         print(f"Epoch {epoch+1} complete. Final Avg Loss for this epoch: {epoch_average_loss}") # average loss across all batches
     
     save_model(model)
-    write_train_loss_to_file(epoch_list, loss_to_plot)
+    write_train_loss_to_file(epoch_list, train_losses)
     validation_losses = validation_loss_checker.current_losses
     print(f"training completed")
     print(f"MODEL TO BE USED FROM EPOCH: {validation_loss_checker.epoch_of_saved_model}")
 
-    return (loss_to_plot, validation_losses, epoch_list)
+    return (train_losses, validation_losses, epoch_list)
 
 def evaluate_model(model, test_data_loader):
     loss_function = BCEWithLogitsLoss()
@@ -249,7 +262,7 @@ if __name__ == "__main__":
     model = BaselineModel()
     if not os.path.exists(f'{MODEL_NAME}/{MODEL_NAME}.pt'):
         print("no previous models, training now")
-        train_losses, validation_losses, epochs = train_model(model, dataloader_for_training, data_loader_for_validation, class_weights)
+        train_losses, validation_losses, epochs = train_model(model, dataloader_for_training, data_loader_for_validation)
         model.load_state_dict(torch.load(f"{MODEL_NAME}/{MODEL_NAME}.pt"))
         plot_loss(train_losses, validation_losses, epochs)
     else:
