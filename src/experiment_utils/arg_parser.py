2197f5b3886fdbf3084e60bc63948b4d5e5df706
Alex George <107772948+algx23@users.noreply.github.com>
2026-01-20 21:29:41 +0000

    Accept arguments for each variable in the augmentation experiments
    Use argparse library to accept flags for:
      - specifying whether any augments will be used
      - specifying whether class weights for weighted BCE will be used
      - flags for each augmentation to be tested:
          - rotation
          - flipping: horizontal and vertical
          - color jitter
          - clahe

--- src/experiment_utils/arg_parser.py
+++ src/experiment_utils/arg_parser.py
@@ -1,3 +1,37 @@
+from argparse import ArgumentParser
+from torchvision.transforms import (RandomRotation,
+                                    RandomHorizontalFlip,
+                                    RandomVerticalFlip,
+                                    ColorJitter)
+
+
 def parse_arguments():
-    print("hello world!")
+    augments_for_experiment = []
+
+    parser = ArgumentParser()
+    # to specify if there is any augment
+    parser.add_argument("--augment", "--aug", action="store_true")
+
+    # to specify if there is a class weight or not
+    parser.add_argument("--class_weights", action="store_true")
+
+    # a flag for each augmentation
+    # Rotation, Horizontal Flip, Vertical Flip, Colour Jitter, CLAHE
+    parser.add_argument("--rotate", choices= [0, 5, 10, 15, 30, 45], default=5)
+    parser.add_argument("--vflip", type=float)
+    parser.add_argument("--hflip", type=float)
+    parser.add_argument("--jitter", action="store_true")
+    parser.add_argument("--clahe", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.vflip is not None and (args.vflip < 0 or args.vflip > 1):
+        raise ValueError("vflip argument must be a floating point number between 0, and 1")
+    
+    if args.hflip is not None and (args.hflip < 0 or args.hflip > 1):
+        raise ValueError("hflip argument must be a floating point number between 0, and 1")
+
+    print(args)
+
     return
+
