399eee64194df4e5bd5f6c2ea2601110d5dd74dd
Alex George <107772948+algx23@users.noreply.github.com>
2026-01-21 17:24:35 +0000

    bug fix: specify type of --rotate argument so that it accepts the parameter properly

--- src/experiment_utils/arg_parser.py
+++ src/experiment_utils/arg_parser.py
@@ -17,7 +17,7 @@ def parse_arguments():
 
     # a flag for each augmentation
     # Rotation, Horizontal Flip, Vertical Flip, Colour Jitter, CLAHE
-    parser.add_argument("--rotate", choices= [0, 5, 10, 15, 30, 45], default=5)
+    parser.add_argument("--rotate", type=int, choices= [0, 5, 10, 15, 30, 45], default=5)
     parser.add_argument("--vflip", type=float)
     parser.add_argument("--hflip", type=float)
     parser.add_argument("--jitter", action="store_true")
