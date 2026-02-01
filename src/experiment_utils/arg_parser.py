74020a86627372f4523be35477c30b1c9cdb3037
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-01 12:52:56 +0000

    --bn flag added to specify whether batch normalization will be utilized in a given experiment

--- src/experiment_utils/arg_parser.py
+++ src/experiment_utils/arg_parser.py
@@ -16,7 +16,8 @@ def parse_arguments():
                     "vflip": "vertical flip: --vflip p : where p is the probability of an image being flipped",
                     "jitter": "color jitter: adjust brightness and contrast by amount provided",
                     "clahe": "if included, use Contrast Limited Histogram Equalization",
-                    "dropout": "if included, dropout layers will be added to the model after the ReLu Activation function is applied"
+                    "dropout": "if included, dropout layers will be added to the model after the ReLu Activation function is applied",
+                    "bn": "if included Batch Normalization will be added to the model"
                     }
     parser.add_argument("--name", required=True, type=str, help=help_strings["name"])
 
@@ -34,8 +35,9 @@ def parse_arguments():
     parser.add_argument("--jitter", type=float, nargs=2, help=help_strings["jitter"])
     parser.add_argument("--clahe", action="store_true", help=help_strings["clahe"])
     parser.add_argument("--dropout", action="store_true", help=help_strings["dropout"])
+    parser.add_argument("--bn", action="store_true", help=help_strings["bn"])
 
-    use_weights, use_clahe, use_dropout = False, False, False
+    use_weights, use_clahe, use_dropout, use_batch_norm = False, False, False, False
     args = parser.parse_args()
 
     if args.vflip is not None and (args.vflip < 0 or args.vflip > 1):
@@ -61,6 +63,8 @@ def parse_arguments():
         use_clahe= True
     if args.dropout:
         use_dropout = True
+    if args.bn:
+        use_batch_norm = True
 
-    return (args.name, augments_for_experiment, use_weights, use_clahe, use_dropout)
+    return (args.name, augments_for_experiment, use_weights, use_clahe, use_dropout, use_batch_norm)
 
