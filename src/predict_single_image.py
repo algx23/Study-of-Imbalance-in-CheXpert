2f9aea2c21e1d6326fffeda0a6a79c9cf17416c4
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-06 19:11:50 +0000

    Refactor: split up classification into multiple functions + error handling
    - Error handling: TypeError raised if image path does not exist, or the
      path provided is not an image - for example, if the user provides a path
      to a CSV
    - Error handling: FileNotFound error raised if the --name flag when running
      the script points to a model which does not exist in results/model_name
    
    - Refactor: classification of one image split into functions to:
        - load and apply transforms to the image
        - perform the classification on the image with the specified model

--- src/predict_single_image.py
+++ src/predict_single_image.py
@@ -1,19 +1,16 @@
 import torch
 from constants import MODEL_NAME, LABELS
+from path_creator import MODEL_ROOT
 from torchvision.transforms import Resize, Normalize, Compose, ToTensor
 import cv2
 from model import BaselineModel
 from PIL import Image
 from tabulate import *
+import os
 
 
-def make_classification(image_path: str): 
-    ''' Classifies a given image based on the chexpert labels
-    Args: image_path [str]: The file path of the image to be classified
-
-    Returns: TODO
-    '''
-    mean =0.5062857270240784
+def prepare_image_for_classification(image_path):
+    mean = 0.5062857270240784
     standard_deviation = 0.2867498937006307
 
     TRANSFORMS = Compose([
@@ -22,15 +19,23 @@ def make_classification(image_path: str):
         Normalize(mean, standard_deviation)
                           ])
 
-    model = BaselineModel(use_dropout=False)
-    model.load_state_dict(torch.load(f"{MODEL_NAME}/{MODEL_NAME}.pt"))
-
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        raise TypeError("No Valid image found at the path provided")
+
     image = Image.fromarray(image)
     image = TRANSFORMS(image)
 
     image = image.unsqueeze(0)
 
+    return image
+
+def make_classification(model, image): 
+    ''' Classifies a given image based on the chexpert labels
+    Args: image_path [str]: The file path of the image to be classified
+
+    Returns: TODO
+    '''
     classifications, HEADERS = [], ["Label", "Prediction", "Confidence"]
 
     model.eval()
@@ -41,12 +46,37 @@ def make_classification(image_path: str):
 
         for label, prediction, confidence_score in zip(LABELS, prediction.tolist()[0], confidence_score.tolist()[0]):
             classifications.append([label, prediction, confidence_score])
-
     
-    print(tabulate(classifications, headers = HEADERS))
+    classification_table = tabulate(classifications, headers = HEADERS)
     
-    return
+    return classification_table
 
-image_path = input("Please Enter the path of the image you would like to classify: \n")
 
-make_classification(image_path)
+def main():
+    # Check if the model name they provided exists already or not
+    path_to_model = MODEL_ROOT / f"{MODEL_NAME}.pt"
+
+    if not os.path.exists(path_to_model):
+        raise FileNotFoundError(f"Model {MODEL_NAME} not found at: {path_to_model}")
+
+    model = torch.load(path_to_model, weights_only=False)
+
+    while True:
+        try:
+            image_path = input("Please Enter the path of the image you would like to classify: \n")
+            image = prepare_image_for_classification(image_path)
+
+            # if the model and image exists, produce the classification
+            print(make_classification(model, image))
+
+            break
+        except TypeError as e:
+            print(f"Error {e}. Try again.")
+            
+        except Exception as e:
+            print(f"Something Unexpexted Went Wrong: {e}")
+        
+
+
+if __name__ == "__main__":
+    main()
