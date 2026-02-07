391c15e0a7dd941c7aee2d7a2797e73a1951e60d
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-07 17:18:14 +0000

    refactor: improve formatting

--- src/data_preparation/sample_dataset_to_5000.py
+++ src/data_preparation/sample_dataset_to_5000.py
@@ -1,11 +1,17 @@
 import pandas as pd
-import os
 from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 
-class DataSubsetter():
+class DataSubsetter:
 
-    def __init__(self, ORIGINAL_DATASET_PATH, TRAIN_SET_PATH, VALIDATION_SET_PATH, TEST_SET_PATH, LABELS):
+    def __init__(
+        self,
+        ORIGINAL_DATASET_PATH,
+        TRAIN_SET_PATH,
+        VALIDATION_SET_PATH,
+        TEST_SET_PATH,
+        LABELS,
+    ):
         self.ORIGINAL_DATASET_PATH = ORIGINAL_DATASET_PATH
         self.TRAIN_SET_PATH = TRAIN_SET_PATH
         self.VALIDATION_SET_PATH = VALIDATION_SET_PATH
@@ -14,7 +20,6 @@ class DataSubsetter():
 
     def create_subset_train_validation(self):
         df = pd.read_csv(self.ORIGINAL_DATASET_PATH)
-
 
         # replace the blank columns for each disease with 0s
         df[self.LABELS] = df[self.LABELS].fillna(0)
@@ -29,24 +34,28 @@ class DataSubsetter():
 
         # see the % of each class in the dataset - before sampling down to 10000
         before_sample_mean = df[self.LABELS].mean(axis=0) * 100
-        print(f"Class Distribution after label cleaning - full dataset: \n{before_sample_mean}")
+        print(
+            f"Class Distribution after label cleaning - full dataset: \n{before_sample_mean}"
+        )
 
         # get a sample of 10k
 
         paths_to_images = df["Path"]
-        split = MultilabelStratifiedShuffleSplit(n_splits=1, test_size=len(paths_to_images.values) - 10000, random_state=0)
+        split = MultilabelStratifiedShuffleSplit(
+            n_splits=1, test_size=len(paths_to_images.values) - 10000, random_state=0
+        )
         # test size is the number of images in the cleaned dataset, - 10,000 so that when MultilabelStratifiedShuffleSplit splits the data, i get 10,000 training images, which is all that is needed, and the rest are set as test images (though with test.csv provided this can be ignored)
 
         # i am only subsetting the train.csv so the test indexes don't really matter
-        for train_index, test_index in split.split(paths_to_images, df[self.LABELS].values):
-            X_train = paths_to_images.iloc[train_index] # stores the paths to the images - essentially the training images
-            Y_train = df[self.LABELS].values[train_index] # stores the label values for each image, so x_train and y_train will line up
-
-        #print(f"Overall Dataset Length: {len(paths_to_images)}")
-        #print(f"Number of || Training Images: {len(X_train)}, Training Labels: {len(Y_train)}")
-        #print(f"Number of || Testing Images: {len(X_test)}, Testing Labels: {len(Y_test)}")
-
-        #print(type(X_train), type(Y_train))
+        for train_index, test_index in split.split(
+            paths_to_images, df[self.LABELS].values
+        ):
+            X_train = paths_to_images.iloc[
+                train_index
+            ]  # stores the paths to the images - essentially the training images
+            Y_train = df[self.LABELS].values[
+                train_index
+            ]  # stores the label values for each image, so x_train and y_train will line up
 
         # when subsett-ing the data, there might be gaps in the row indexes in the numpy array
         # eg, one example might be from row 2222, but the next example taken as part of
@@ -56,25 +65,35 @@ class DataSubsetter():
         # blank
         # so i reset the indexes, so they both line up starting from 0
         # source: https://www.datacamp.com/tutorial/pandas-reset-index-tutorial
-        subset_df = X_train.to_frame().reset_index(drop=True) # drop so the old indexes arent a column
+        subset_df = X_train.to_frame().reset_index(
+            drop=True
+        )  # drop so the old indexes arent a column
         subset_df[self.LABELS] = pd.DataFrame(Y_train, columns=self.LABELS)
 
         subset_df.to_csv("subset.csv", index=False)
 
         # class distribution of the subset
-        subset_mean = subset_df[self.LABELS].mean(axis=0) * 100
+        subset_mean = subset_df[self.LABELS].mean() * 100
         print(f"class distribution after sampling: \n{subset_mean}")
 
         # splitting into validatation set for early stopping - 90/10
-        paths_to_training_images = subset_df['Path']
-        validation_split = MultilabelStratifiedShuffleSplit(n_splits=1, test_size=1000, random_state=0)
+        paths_to_training_images = subset_df["Path"]
+        validation_split = MultilabelStratifiedShuffleSplit(
+            n_splits=1, test_size=1000, random_state=0
+        )
 
-        for train_index, test_index in validation_split.split(paths_to_training_images, subset_df[self.LABELS].values):
+        for train_index, test_index in validation_split.split(
+            paths_to_training_images, subset_df[self.LABELS].values
+        ):
 
-            X_train = paths_to_training_images.iloc[train_index] # stores the paths to the images - essentially the training images
+            X_train = paths_to_training_images.iloc[
+                train_index
+            ]  # stores the paths to the images - essentially the training images
             X_train_val = paths_to_training_images.iloc[test_index]
-            Y_train = subset_df[self.LABELS].values[train_index] # stores the label values for each image, so x_train and y_train will line up
-            Y_train_val = subset_df[self.LABELS].values[test_index] 
+            Y_train = subset_df[self.LABELS].values[
+                train_index
+            ]  # stores the label values for each image, so x_train and y_train will line up
+            Y_train_val = subset_df[self.LABELS].values[test_index]
 
         training_df = X_train.to_frame().reset_index(drop=True)
         training_df[self.LABELS] = pd.DataFrame(Y_train, columns=self.LABELS)
@@ -85,9 +104,10 @@ class DataSubsetter():
         training_df.to_csv(self.TRAIN_SET_PATH, index=False)
         validation_df.to_csv(self.VALIDATION_SET_PATH, index=False)
 
-
-        validation_mean = validation_df[self.LABELS].mean(axis=0) * 100
-        print(f"class distribution after sampling validation: \n{round(validation_mean, 2)}")
+        validation_mean = validation_df[self.LABELS].mean() * 100
+        print(
+            f"class distribution after sampling validation: \n{round(validation_mean, 2)}"
+        )
 
         return
 
@@ -101,17 +121,26 @@ class DataSubsetter():
         # either set - prevent leakage
 
         # adapted from: https://stackoverflow.com/questions/44706485/how-to-remove-rows-in-a-pandas-dataframe-if-the-same-row-exists-in-another-dataf
-        df_train_removed = pd.merge(all_df, train_df, indicator=True, how="outer").query("_merge=='left_only'").drop("_merge", axis=1)
-        df_fully_removed = pd.merge(df_train_removed, valid_df, indicator=True, how="outer").query("_merge=='left_only'").drop("_merge", axis=1)
+        df_train_removed = (
+            pd.merge(all_df, train_df, indicator=True, how="outer")
+            .query("_merge=='left_only'")
+            .drop("_merge", axis=1)
+        )
+        df_fully_removed = (
+            pd.merge(df_train_removed, valid_df, indicator=True, how="outer")
+            .query("_merge=='left_only'")
+            .drop("_merge", axis=1)
+        )
 
         # split this to get the test set
         paths_to_images = df_fully_removed["Path"]
         labels_for_each_row = df_fully_removed[self.LABELS].values
-        
 
-        test_split = MultilabelStratifiedShuffleSplit(n_splits=1, test_size=1000, random_state=0)
+        test_split = MultilabelStratifiedShuffleSplit(
+            n_splits=1, test_size=1000, random_state=0
+        )
         for _, test_index in test_split.split(paths_to_images, labels_for_each_row):
-            X_test = paths_to_images.iloc[test_index] 
+            X_test = paths_to_images.iloc[test_index]
             Y_test = labels_for_each_row[test_index]
 
         print(type(X_test))
@@ -125,8 +154,4 @@ class DataSubsetter():
         print(test_df[self.LABELS].mean() * 100)
         test_df.to_csv(self.TEST_SET_PATH)
 
-
         return
-
-
-
