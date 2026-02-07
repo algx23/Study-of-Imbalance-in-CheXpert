7eeaf862d51813d9d56152a04523ba180afd1df0
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-07 18:36:47 +0000

    refactor: format and add a more informative error message if the model is not found

--- src/predict_single_image.py
+++ src/predict_single_image.py
@@ -1,9 +1,8 @@
 import torch
-from constants import MODEL_NAME, LABELS
-from path_creator import MODEL_ROOT
+from constants.control_variables import MODEL_NAME, LABELS
+from constants.paths import MODEL_ROOT
 from torchvision.transforms import Resize, Normalize, Compose, ToTensor
 import cv2
-from model import BaselineModel
 from PIL import Image
 from tabulate import *
 import os
@@ -13,11 +12,9 @@ def prepare_image_for_classification(image_path):
     mean = 0.5062857270240784
     standard_deviation = 0.2867498937006307
 
-    TRANSFORMS = Compose([
-        Resize((224, 224)),
-        ToTensor(),
-        Normalize(mean, standard_deviation)
-                          ])
+    TRANSFORMS = Compose(
+        [Resize((224, 224)), ToTensor(), Normalize(mean, standard_deviation)]
+    )
 
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     if image is None:
@@ -30,25 +27,30 @@ def prepare_image_for_classification(image_path):
 
     return image
 
-def make_classification(model, image): 
-    ''' Classifies a given image based on the chexpert labels
+
+def make_classification(model, image):
+    """Classifies a given image based on the chexpert labels
     Args: image_path [str]: The file path of the image to be classified
 
-    Returns: TODO
-    '''
+    Returns:
+    """
     classifications, HEADERS = [], ["Label", "Prediction", "Confidence"]
 
     model.eval()
     with torch.no_grad():
         output = model(image)
-        confidence_score = torch.sigmoid(output) # turns the raw output into a range 0 - 1
+        confidence_score = torch.sigmoid(
+            output
+        )  # turns the raw output into a range 0 - 1
         prediction = (confidence_score > 0.5).int()
 
-        for label, prediction, confidence_score in zip(LABELS, prediction.tolist()[0], confidence_score.tolist()[0]):
+        for label, prediction, confidence_score in zip(
+            LABELS, prediction.tolist()[0], confidence_score.tolist()[0]
+        ):
             classifications.append([label, prediction, confidence_score])
-    
-    classification_table = tabulate(classifications, headers = HEADERS)
-    
+
+    classification_table = tabulate(classifications, headers=HEADERS)
+
     return classification_table
 
 
@@ -57,13 +59,22 @@ def main():
     path_to_model = MODEL_ROOT / f"{MODEL_NAME}.pt"
 
     if not os.path.exists(path_to_model):
-        raise FileNotFoundError(f"Model {MODEL_NAME} not found at: {path_to_model}")
+        results_folder_exists = os.path.exists("results")
+        error_msg = (
+            f"Model `{MODEL_NAME}` not found at: {path_to_model}. "
+            f"Run [py main.py --name {MODEL_NAME}] to train, then return, or select an existing model. "
+            f"You can select from: {os.listdir("results") if results_folder_exists else 
+                                    'No Models Exist. Please train a model: py main.py --name NAME'}"
+        )
+        raise FileNotFoundError(error_msg)
 
     model = torch.load(path_to_model, weights_only=False)
 
     while True:
         try:
-            image_path = input("Please Enter the path of the image you would like to classify: \n")
+            image_path = input(
+                "Please Enter the path of the image you would like to classify: \n"
+            )
             image = prepare_image_for_classification(image_path)
 
             # if the model and image exists, produce the classification
@@ -72,10 +83,9 @@ def main():
             break
         except TypeError as e:
             print(f"Error {e}. Try again.")
-            
+
         except Exception as e:
             print(f"Something Unexpexted Went Wrong: {e}")
-        
 
 
 if __name__ == "__main__":
