e6083367158e8282aec0c2bc957b5b2ade6f1283
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-10 10:56:29 +0000

    Add Script to run experiments - for demo only 4 have been run, full runs to commence after

---
+++ src/experiments.ps1
@@ -0,0 +1,42 @@
+# Testing the baseline: raw model performance, with no measures to help imbalance
+python main.py --name baseline
+Start-Sleep -Seconds 300
+
+# Loss Level change to address imbalance
+python main.py --name baseline_with_weighted_loss --class_weights 
+Start-Sleep -Seconds 300
+
+# Model Architecture level changes to deal with Inter-class Similarity and Intra-Class Variance
+python main.py --name baseline_with_weights_dropout --class_weights --dropout
+
+# Data Level Changes to help - Imbalanced data means the model sees more images of one class than another..does making the model see different images make the model generalize better to these classes
+#     - Rotation
+      python main.py --name rotation --rotate 5 --class_weights --dropout
+      Start-Sleep -Seconds 300
+
+      python main.py --name rotation_10 --rotate 10 --class_weights --dropout
+      Start-Sleep -Seconds 300
+
+      python main.py --name rotation_45 --rotate 45 --class_weights --dropout
+      Start-Sleep -Seconds 300
+
+#     - Flipping
+     python main.py --name horizontal_flip --hflip --class_weights --dropout
+     Start-Sleep -Seconds 300
+     python main.py --name vertical_flip --vflip --class_weights --dropout
+     Start-Sleep -Seconds 300
+
+  # Photometric Transformations: changing contrast
+    python main.py --name jitter --jitter 0.3 0.3 --class_weights --dropout
+    Start-Sleep -Seconds 300
+
+    python main.py --name clahe --clahe --class_weights --dropout
+    Start-Sleep -Seconds 300
+
+
+
+
+
+
+
+
