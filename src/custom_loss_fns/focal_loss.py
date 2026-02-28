74aa36245daf35d8ec2fb4c6e6e7b1713059e472
Alex George <107772948+algx23@users.noreply.github.com>
2026-02-28 13:12:47 +0000

    Initial commit - skeleton of focal loss class

---
+++ src/custom_loss_fns/focal_loss.py
@@ -0,0 +1,15 @@
+class FocalLoss():
+    """
+    A custom Focal Loss Implementation for multi-label classification
+
+    References:
+    - Focal Loss for Dense Object Detection [https://arxiv.org/pdf/1708.02002]
+    - the idea for indicator functions: https://math.stackexchange.com/questions/5101858/how-do-i-write-a-piecewise-function-in-a-single-equation
+    - Indicator Functions (to turn the focal loss into one equation): [https://en.wikipedia.org/wiki/Indicator_function]
+    """
+    # TODO: setup focal loss
+    def __init__(self) -> None:
+        pass
+
+    def calculate_focal_loss():
+        return NotImplemented
