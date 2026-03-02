648f880bf88a079ea721980dba334e54d8c862e3
Alex George <107772948+algx23@users.noreply.github.com>
2026-03-02 11:06:20 +0000

    Change Focal loss class definition to inherit from nn.module()
    Making the focal loss class inherit from nn.module() and renaming the method to forward to follow the standard implementation guidelines for custom loss functions/classes means the training/evaluation loop logic can stay the same

--- src/custom_loss_fns/focal_loss.py
+++ src/custom_loss_fns/focal_loss.py
@@ -1,9 +1,10 @@
 from torch import Tensor
 import torch
 from torch.nn import BCEWithLogitsLoss
+import torch.nn as nn
 
 
-class FocalLoss():
+class FocalLoss(nn.Module):
     """
     A custom Focal Loss Implementation for multi-label classification
 
@@ -12,14 +13,18 @@ class FocalLoss():
     - the idea for indicator functions: https://math.stackexchange.com/questions/5101858/how-do-i-write-a-piecewise-function-in-a-single-equation
     - Indicator Functions (to turn the focal loss into one equation): [https://en.wikipedia.org/wiki/Indicator_function]
     """
+
     # TODO: setup focal loss
-    def __init__(self, alpha: Tensor, gamma: float)  -> None:
+    def __init__(self, alpha: Tensor, gamma: float) -> None:
+        super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
-        self.bce_loss = BCEWithLogitsLoss() # needed to calculate the log(p) part of the equation
+        self.bce_loss = BCEWithLogitsLoss(
+            reduction="none"
+        )  # needed to calculate the log(p) part of the equation
         return
 
-    def calculate_focal_loss(self, logits: Tensor, true_labels: Tensor) -> Tensor:
+    def forward(self, logits: Tensor, true_labels: Tensor) -> Tensor:
         """
         Calculates the focal loss of a given batch
         p_t = p if y=1
@@ -33,8 +38,11 @@ class FocalLoss():
         """
 
         p = torch.sigmoid(logits)
-        ce = self.bce_loss(logits, true_labels, reduction="none")
+        ce = self.bce_loss(logits, true_labels)
         # compute the loss across all classes and then return it as a single value
         # so that it fits with my existing training loop
-        focal_loss = ce*(self.alpha*true_labels*(1-p)**self.gamma + (1-self.alpha)*(1-true_labels)*p**self.gamma)
-        return focal_loss.mean()
+        focal_loss = ce * (
+            self.alpha * true_labels * (1 - p) ** self.gamma
+            + (1 - self.alpha) * (1 - true_labels) * p**self.gamma
+        )
+        return torch.mean(focal_loss)
