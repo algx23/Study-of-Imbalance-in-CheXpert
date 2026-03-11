d2f5bb909a2a9a362f183f25a4ded5154094f759
Alex George <107772948+algx23@users.noreply.github.com>
2026-03-11 09:05:05 +0000

    Change all experiments to use the --threshold=fixed flag
    If time allows (it likely will), I will also conduct all experiments again at the optimal threshold which can allow me to compare more
    
    The plan at the moment is to compare thresholds of some models across some classes

--- src/experiments.ps1
+++ src/experiments.ps1
@@ -1,39 +1,46 @@
 # Testing the baseline: raw model performance, with no measures to help imbalance
-python main.py --name baseline
+python main.py --name baseline --threshold=fixed
 Start-Sleep -Seconds 300
 
 # Loss Level change to address imbalance
-python main.py --name baseline_with_weighted_loss --class_weights 
+python main.py --name baseline_with_weighted_loss --class_weights --threshold=fixed
 Start-Sleep -Seconds 300
 
-python main.py --name baseline_focal_loss --focal_loss
+python main.py --name baseline_focal_loss --focal_loss --threshold=fixed
+Start-Sleep -Seconds 300
+
+python main.py --name baseline_cbfl --balanced_focal_loss --threshold=fixed
 Start-Sleep -Seconds 300
 
 # Model Architecture level changes to deal with Inter-class Similarity and Intra-Class Variance
-python main.py --name baseline_with_weights_dropout --class_weights --dropout
+python main.py --name baseline_with_weights_dropout --class_weights --dropout --threshold=fixed
+Start-Sleep -Seconds 300
+
+python main.py --name baseline_with_weights_bn --class_weights --bn --threshold=fixed
+Start-Sleep -Seconds 300
 
 # Data Level Changes to help - Imbalanced data means the model sees more images of one class than another..does making the model see different images make the model generalize better to these classes
 #     - Rotation
-      python main.py --name rotation_5 --rotate 5 --class_weights --dropout
+      python main.py --name rotation_5 --rotate 5 --class_weights --dropout --threshold=fixed
       Start-Sleep -Seconds 300
 
-      python main.py --name rotation_10 --rotate 10 --class_weights --dropout
+      python main.py --name rotation_10 --rotate 10 --class_weights --dropout --threshold=fixed
       Start-Sleep -Seconds 300
 
-      python main.py --name rotation_45 --rotate 45 --class_weights --dropout
+      python main.py --name rotation_45 --rotate 45 --class_weights --dropout --threshold=fixed
       Start-Sleep -Seconds 300
 
 #     - Flipping
-     python main.py --name horizontal_flip --hflip 0.5 --class_weights --dropout
+     python main.py --name horizontal_flip --hflip 0.5 --class_weights --dropout --threshold=fixed
      Start-Sleep -Seconds 300
-     python main.py --name vertical_flip --vflip 0.5 --class_weights --dropout
+     python main.py --name vertical_flip --vflip 0.5 --class_weights --dropout --threshold=fixed
      Start-Sleep -Seconds 300
 
   # Photometric Transformations: changing contrast
-    python main.py --name jitter --jitter 0.3 0.3 --class_weights --dropout
+    python main.py --name jitter --jitter 0.3 0.3 --class_weights --dropout --threshold=fixed
     Start-Sleep -Seconds 300
 
-    python main.py --name clahe --clahe --class_weights --dropout
+    python main.py --name clahe --clahe --class_weights --dropout --threshold=fixed
     Start-Sleep -Seconds 300
 
 
